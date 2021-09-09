6987f0d38249c3cf8546f6871cb41f02ac38e209
Red-Asuka <2586632362@qq.com>
2021-09-09 16:54:41 +0800

    improve(config): improve products github config

--- .vuepress/config/githubConfig.js
+++ .vuepress/config/githubConfig.js
@@ -15,12 +15,91 @@ const EMQXConfig = {
   },
 }
 
+const cloudConfig = {
+  cn: {
+    docName: 'Cloud 文档',
+    docsRepo: 'emqx/cloud-docs',
+    docsDir: 'zh_CN',
+  },
+  en: {
+    docName: 'Cloud Docs',
+    docsRepo: 'emqx/cloud-docs',
+    docsDir: 'en_US',
+  },
+}
+
+const fabricConfig = {
+  cn: {
+    docName: 'Fabric 文档',
+    docsRepo: 'emqx/fabric-docs',
+    docsDir: 'zh_CN',
+  },
+  en: {
+    docName: 'Fabric Docs',
+    docsRepo: 'emqx/fabric-docs',
+    docsDir: 'en_US',
+  },
+}
+
+const edgeConfig = {
+  cn: {
+    docName: 'Edge 文档',
+    docsRepo: 'emqx/emqx-edge-docs',
+    docsDir: 'zh_CN',
+  },
+  en: {
+    docName: 'Edge Docs',
+    docsRepo: 'emqx/emqx-edge-docs',
+    docsDir: 'en_US',
+  },
+}
+
+const kuiperConfig = {
+  cn: {
+    docName: 'eKuiper 文档',
+    docsRepo: 'lf-edge/ekuiper',
+    docsDir: 'docs/zh_CN',
+  },
+  en: {
+    docName: 'eKuiper Docs',
+    docsRepo: 'lf-edge/ekuiper',
+    docsDir: 'docs/en_US',
+  },
+}
+
+const neuronConfig = {
+  cn: {
+    docName: 'Neuron 文档',
+    docsRepo: 'neugates/neuron-docs',
+    docsDir: 'zh_CN',
+  },
+  en: {
+    docName: 'Neuron Docs',
+    docsRepo: 'neugates/neuron-docs',
+    docsDir: 'en_US',
+  },
+}
+
 let currentConfig
 switch (docsType) {
   case 'emqx':
     currentConfig = EMQXConfig
     break
-
+  case 'cloud':
+    currentConfig = cloudConfig
+    break
+  case 'fabric':
+    currentConfig = fabricConfig
+    break
+  case 'edge':
+    currentConfig = edgeConfig
+    break
+  case 'kuiper':
+    currentConfig = kuiperConfig
+    break
+  case 'neuron':
+    currentConfig = neuronConfig
+    break
   default:
     currentConfig = EMQXConfig
 }
